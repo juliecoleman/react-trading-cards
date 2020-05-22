64780ecac763f93008528ae247ef92b38f16773b
Julie Coleman <juliecoleman820@gmail.com>
2020-05-22 00:30:45 +0000

    Added single parent component

--- static/js/tradingCard.jsx
+++ static/js/tradingCard.jsx
@@ -1,72 +1,5 @@
 "use strict";
 
-class TradingCard extends React.Component {
-  render() {
-    return (
-      <div className="card">
-        <h2>Name: {this.props.name}</h2>
-        <img src={this.props.imgUrl} />
-        <h2>Skill:{this.props.skill}</h2>
-      </div>
-    );
-  }
-}
-
-ReactDOM.render(
-  (
-    <TradingCard
-      name="Balloonicorn"
-      skill="video games"
-      imgUrl="/static/img/balloonicorn.jpg"
-    />
-  ),
-  document.querySelector('#balloonicorn')
-);
-
-ReactDOM.render(
-  (
-    <TradingCard
-      name="Float"
-      skill="baking pretzels"
-      imgUrl="/static/img/float.jpg"
-    />
-  ),
-  document.querySelector('#float')
-);
-
-ReactDOM.render(
-  (
-    <TradingCard
-      name="Llambda"
-      skill="knitting scarves"
-      imgUrl="/static/img/llambda.jpg"
-    />
-  ),
-  document.querySelector('#llambda')
-);
-
-ReactDOM.render(
-  (
-    <TradingCard
-      name="Merge"
-      skill="scrabble"
-      imgUrl="/static/img/merge.jpg"
-    />
-  ),
-  document.querySelector('#merge')
-);
-
-ReactDOM.render(
-  (
-    <TradingCard
-      name="Polymorphism"
-      skill="spelling backwards"
-      imgUrl="/static/img/polymorphism.jpg"
-    />
-  ),
-  document.querySelector('#polymorphism')
-);
-
 const tradingCardData = [
   {
     name: 'Balloonicorn',
@@ -117,3 +50,47 @@ const tradingCardData = [
     imgUrl: '/static/img/merge.jpg'
   }
 ];
+
+
+class TradingCard extends React.Component {
+  render() {
+    return (
+      <div className="card">
+        <h2>Name: {this.props.name}</h2>
+        <img src={this.props.imgUrl} />
+        <h2>Skill:{this.props.skill}</h2>
+      </div>
+    );
+  }
+}
+
+class TradingCardContainer extends React.Component {
+  render() {
+    const tradingCards = [];
+
+    for (const currentCard of tradingCardData) {
+      tradingCards.push(
+      <TradingCard
+        name={currentCard.name}
+        skill={currentCard.skill}
+        imgUrl={currentCard.imgUrl}
+      />
+      );
+    }
+
+    return (
+      <div>
+        {tradingCards}
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(
+  <TradingCardContainer />,
+  document.querySelector('#root')
+);
+
+
+    
+
